831996ff0e290c343b22ce64ed0de907d284f213
Meanhor.Kea <meanhor1904@gmail.com>
2024-07-03 23:45:31 +0700

    update the image dataset

--- data/index.ts
+++ data/index.ts
@@ -13,7 +13,7 @@ export const gridItems = [
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/b1.svg",
+    img: "/design1.svg",
     spareImg: "",
   },
   {
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
+    title: "Studying web development and artificial intelligence concepts.",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
@@ -72,32 +72,32 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    title: "TenantTrack-Web",
+    des: "Simplify rental management with TenantTrack. Effortless rent collection, seamless tenant communication, and actionable data empower you to thrive. Focus on what matters most - growing your rental business with confidence.",
     img: "/p1.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "https://github.com/adrianhajdin?tab=repositories",
+    link: "https://github.com/Meanhor/TenantTrack-Web.git",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    title: "Expense-Tracking-React",
+    des: "This application helps you manage your expenses by providing an intuitive user interface to display and track transactions, as well as showing your overall balance, total expenses, and income. You can also add new transactions and delete existing ones.",
     img: "/p2.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/zoom-clone",
+    link: "https://github.com/Meanhor/Expense-Tracking-React.git",
   },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    title: "React Weather App",
+    des: "A simple weather application built with React that fetches current weather data using the OpenWeatherMap API.",
     img: "/p3.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/ai_saas_app",
+    link: "https://github.com/Meanhor/Weather-App-React.git",
   },
   {
     id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    title: "Portfolio-Next",
+    des: "This repository contains a personal portfolio website built using Next.js and Tailwind CSS. It serves as a showcase for my skills, projects, and professional experience in web development.",
     img: "/p4.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
     link: "https://github.com/adrianhajdin/iphone",
@@ -107,66 +107,54 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "Working with Kea Meanhor was a pleasure. Their thorough understanding of web development and their dedication to delivering excellent results were evident in every project. Kea's enthusiasm for learning and their ability to tackle challenges creatively make them a standout student.",
+    name: "Dr. Jason Wilson",
+    title: "Artificial Intelligence Lecturer",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "I am pleased to recommend Kea Meanhor, who has consistently demonstrated exceptional skills in programming and software development. Their proactive approach, analytical mindset, and commitment to excellence set them apart as a valuable asset.",
+    name: "Dr. David Parker",
+    title: "Computer Science Instructor",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "Kea Meanhor showed remarkable proficiency in web technologies and demonstrated a strong work ethic throughout their coursework. Their passion for coding and problem-solving abilities make them well-suited for any future endeavors in computer science.",
+    name: "Dr. Alexander Fraud",
+    title: "Web Development Professor",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "Working with Kea Meanhor has been a pleasure. Their deep understanding of software engineering principles and their commitment to delivering outstanding results were evident in every project. Kea's enthusiasm for learning and their ability to think critically make them a standout student.",
+    name: "Prof. Emily Johnson",
+    title: "Computer Science Lecturer",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "I am pleased to recommend Kea Meanhor, who has consistently demonstrated exceptional skills in programming and algorithm design. Their proactive approach, strong problem-solving abilities, and dedication to continuous improvement set them apart.",
+    name: "Dr. Sarah Thompson",
+    title: "Data Structure & Algorithm Instructor",
   },
 ];
 
 export const companies = [
   {
     id: 1,
-    name: "cloudinary",
-    img: "/cloud.svg",
-    nameImg: "/cloudName.svg",
+    name: "",
+    img: "/re.svg",
+    nameImg: "/React.svg",
   },
   {
     id: 2,
-    name: "appwrite",
-    img: "/app.svg",
-    nameImg: "/appName.svg",
+    name: "",
+    img: "/tail.svg",
+    nameImg: "/tailwind.svg",
   },
   {
     id: 3,
-    name: "HOSTINGER",
-    img: "/host.svg",
-    nameImg: "/hostName.svg",
-  },
-  {
-    id: 4,
-    name: "stream",
-    img: "/s.svg",
-    nameImg: "/streamName.svg",
-  },
-  {
-    id: 5,
-    name: "docker.",
-    img: "/dock.svg",
-    nameImg: "/dockerName.svg",
+    name: "",
+    img: "/ts.svg",
+    nameImg: "/type.svg",
   },
 ];
 
@@ -180,22 +168,22 @@ export const workExperience = [
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Experienced UI Designer for Web and Mobile",
+    desc: "With two years of experience, adept at designing intuitive UIs for web and mobile platforms.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Diverse Event Organizer",
+    desc: "Experienced in organizing a variety of events, including those for business, government, and community projects.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Craftsperson of Machine Learning",
+    desc: "Discover the skills and techniques essential for crafting and deploying machine learning models.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
